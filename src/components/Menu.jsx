7d3f6bdabdce8f53ce745946fd4c247740778b37
paul-nguyen96 <paul121596@icloud.com>
2023-02-10 20:46:59 -0600

    active menu button open/close w/ outside clicks

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,31 +1,69 @@
-import React from "react";
-import { Button, Separator, MenuList, MenuListItem} from "react95";
+import React, { useEffect, useRef, useState } from "react";
+import { Button, Separator, MenuList, MenuListItem } from "react95";
 
 function Menu() {
+  const [showMenu, setShowMenu] = useState(false);
+
+  //Display Menu when clicked
+  const handleActiveMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
+  //Close menu when clicking outside menu
+  const ref = useRef()
+  useEffect(() => {
+    const checkOutsideClick = e => {
+      if(showMenu && ref.current && !ref.current.contains(e.target)){
+        setShowMenu(!showMenu)
+      }
+    }
+    document.addEventListener("mousedown", checkOutsideClick)
+    return () => {
+      document.removeEventListener("mousedown", checkOutsideClick)
+    }
+  }, [showMenu])
+
+ 
+
   return (
-    <div style={{ position: "relative", display: "inline-block" }}>
-      <MenuList style={{ position: "absolute", left: "0", top: "100%" }}>
-        <MenuListItem>
-          <img
-            style={{ width: 22, marginRight: 8 }}
-            src={require("../assets/file.png")}
-            alt="aboutLogo"
-          />
-          <span>GitHub Repo</span>
-        </MenuListItem>
-        <MenuListItem>
-          <img
-            style={{ width: 22, marginRight: 8 }}
-            src={require("../assets/computer.png")}
-            alt="aboutLogo"
-          />
-          <span>About</span>
-        </MenuListItem>
-        <Separator />
-        <MenuListItem>
-        Start
-        </MenuListItem>
-      </MenuList>
+    <div
+    ref={(ref)}
+      onClick={handleActiveMenu}
+      style={{ position: "relative", display: "inline-block" }}
+    >
+      {showMenu && (
+        <MenuList
+          style={{
+            position: "absolute",
+            left: "0",
+            top: "100%",
+          }}
+        >
+          <MenuListItem>
+            <img
+              style={{ width: 22, marginRight: 8 }}
+              src={require("../assets/file.png")}
+              alt="aboutLogo"
+            />
+            <a
+              href="https://github.com/paul-nguyen96/Recipe95"
+              style={{ cursor: "default" }}
+            >
+              GitHub Repo
+            </a>
+          </MenuListItem>
+          <MenuListItem>
+            <img
+              style={{ width: 22, marginRight: 8 }}
+              src={require("../assets/computer.png")}
+              alt="aboutLogo"
+            />
+            <span>About</span>
+          </MenuListItem>
+          <Separator />
+          <MenuListItem>Start</MenuListItem>
+        </MenuList>
+      )}
       <Button style={{ fontWeight: "bold", marginRight: 6 }}>
         <img
           src={require("../assets/windows.png")}
